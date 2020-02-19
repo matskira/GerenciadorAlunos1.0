c00b5d7c847f28e8ed13b348e8a2a066dfa6e9ec
Matheus Mariano Poda <mpoda@server01.foursys.local>
2020-02-19 16:52:13 -0300

    att

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -5,16 +5,29 @@ public class Aluno {
 	private String nomeAluno;
 	private char sexo;
 	private Data dataNascimento;
+	private String rg;
+	private String cpf;
+	private Endereco endereco;
+	private String telefone;
+	private String senha;
 	
 	public Aluno() {
 	}
 
-	public Aluno(String numeroMatricula, String nomeAluno, char sexo, Data dataNascimento) {
+	
+	public Aluno(String numeroMatricula, String nomeAluno, char sexo, Data dataNascimento, String rg, String cpf,
+			Endereco endereco, String telefone, String senha) {
 		this.numeroMatricula = numeroMatricula;
 		this.nomeAluno = nomeAluno;
 		this.sexo = sexo;
 		this.dataNascimento = dataNascimento;
+		this.rg = rg;
+		this.cpf = cpf;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.senha = senha;
 	}
+
 
 	public String getNumeroMatricula() {
 		return numeroMatricula;
@@ -47,6 +60,53 @@ public class Aluno {
 	public void setDataNascimento(Data dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
 	
 	
 	
